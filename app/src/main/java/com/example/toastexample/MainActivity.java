7dc253b7db93d6602010644e1e3b2846084257d2
Boni Octavianus <bonioctavianus.31@gmail.com>
2023-03-14 21:01:48 +0700

    toast example

---
+++ app/src/main/java/com/example/toastexample/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.toastexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(
+                this,
+                "Hello World",
+                Toast.LENGTH_LONG
+        ).show();
+    }
+}
